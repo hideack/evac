6c043187256192f1963d5acec1c0a7099a01e103
hideack <hide.terai@gmail.com>
2014-10-05 11:51:24 +0900

    監視先のディレクトリを追加

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gutil = require('gulp-util');
 gulp.task('mocha', function() {
   require('chai').should();
 
-  return gulp.src(['spec/plugin/in/*.js', 'spec/plugin/filter/*.js'], { read: false })
+  return gulp.src(['spec/plugin/in/*.js', 'spec/plugin/filter/*.js', 'spec/plugin/out/*.js'], { read: false })
     .pipe(mocha({ reporter: 'list'}))
     .on('error', gutil.log);
 });
