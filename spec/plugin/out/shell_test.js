e91d53445a35313e3e7341acc5efab604df98b3d
hideack <hide.terai@gmail.com>
2014-10-18 22:05:33 +0900

    shellプラグインのテスト修正

--- spec/plugin/out/shell_test.js
+++ spec/plugin/out/shell_test.js
@@ -2,11 +2,26 @@ var shell = require('../../../lib/plugin/out/shell.js'),
     nock = require('nock');
 
 describe('output plugin: shell', function(){
-  it('should be echo command successful.', function(){
+  it('should be echo command successful.', function(done){
     var args = {
       "command": "echo %s"
     };
 
-    shell.output(args, "test");
+    shell.output(args, "test", function(err, output){
+      err.should.be.false;
+      output.should.be.equal("test\n");
+      done();
+    });
+  });
+
+  it('should be command fails.', function(done){
+    var args = {
+      "command": "hogehoge %s"
+    };
+
+    shell.output(args, "test", function(err, output){
+      err.should.be.true;
+      done();
+    });
   });
 });
