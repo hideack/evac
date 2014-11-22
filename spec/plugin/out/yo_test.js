838fff346b5af0d4f22d7d1d488d6cb80c5c41a5
hideack <hide.terai@gmail.com>
2014-11-22 14:48:50 +0900

    Add Yo plugin test.

---
+++ spec/plugin/out/yo_test.js
@@ -0,0 +1,31 @@
+var yo = require('../../../lib/plugin/out/yo.js'),
+    nock = require('nock');
+
+describe('output plugin: yo', function(){
+  it('should be Yo successful.', function(done){
+    nock("http://api.justyo.co").post('/yoall/').reply(200);
+
+    var args = {
+      "api_token": "foobar"
+    };
+
+    yo.output(args, "test", function(err, output){
+      err.should.be.false;
+      done();
+    });
+  });
+
+  it('should be Yo fail.', function(done){
+    nock("http://api.justyo.co").post('/yoall/').reply(201);
+
+    var args = {
+      "api_token": "foobar"
+    };
+
+    yo.output(args, "test", function(err, output){
+      err.should.be.true;
+      done();
+    });
+  });
+
+});
