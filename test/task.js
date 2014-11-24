3c95b7ce03e9919bda7f0d01b73b5164920f440b
hideack <hide.terai@gmail.com>
2014-11-24 15:09:18 +0900

    メイン呼び出し処理のテスト追加

---
+++ test/task.js
@@ -0,0 +1,61 @@
+var core = require('../lib/core');
+
+describe('evac tasks', function(){
+  describe('parseRecipeJson()', function(){
+    it('should be parse recipe.', function(done){
+      var recipe = {
+        "in": {
+          "staticWord": {
+            "text": "test word."
+          }
+        },
+        "filter": {
+          "through": {}
+        },
+        "out": {
+          "stdout": {}
+        }
+      };
+
+      core.parseRecipeJson(recipe, function(err, message){
+        err.should.be.false;
+        done();
+      });
+    });
+
+    it('should be parse abbreviated recipe.', function(done){
+      var recipe = {
+        "in": {
+          "staticWord": {
+            "text": "test word."
+          }
+        },
+        "out": {
+          "stdout": {}
+        }
+      };
+
+      core.parseRecipeJson(recipe, function(err, message){
+        err.should.be.false;
+        done();
+      });
+    });
+
+
+    it('should be raise error with invalid recipe.', function(done){
+      var recipe = {
+        "inin": {
+          "staticWord": {
+            "text": "test word."
+          }
+        }
+      };
+
+      core.parseRecipeJson(recipe, function(err, message){
+        err.should.be.not.false;
+        done();
+      });
+    });
+
+  });
+});
