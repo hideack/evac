24507d52ca2df7945421e982589886e5cabcc021
hideack <hide.terai@gmail.com>
2015-08-01 22:31:01 +0900

    環境に応じてテスト切り替え

--- test/plugin/in/battery_test.js
+++ test/plugin/in/battery_test.js
@@ -1,10 +1,16 @@
 var battery = require('../../../lib/plugin/in/battery.js');
 
 describe('input plugin: battery', function(){
-  it('should received current battery level.', function(done){
-    battery.load({}, function(error, output){
-      output[0].should.be.a('number');
+  if (process.platform == 'darwin') {
+    it('should received current battery level.', function(done){
+      battery.load({}, function(error, output){
+        output[0].should.be.a('number');
+        done();
+      });
+    });
+  } else {
+    it.skip('should received current battery level.', function(done){
       done();
     });
-  });
+  }
 });
