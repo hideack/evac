b620183d79929a96e8d4cc1e9c4f779a7b1984fc
hideack <hide.terai@gmail.com>
2016-08-20 13:23:14 +0900

    Fix sepia called timing.

--- test/plugin/out/gaer_test.js
+++ test/plugin/out/gaer_test.js
@@ -1,11 +1,11 @@
-var sepia = require('sepia'),
-    gaer = require('../../../lib/plugin/out/gaer.js'),
+var gaer = require('../../../lib/plugin/out/gaer.js'),
     path = require('path');
-
-sepia.fixtureDir(path.join(process.cwd(), 'test', 'sepia-fixtures'));
 
 describe('output plugin: gaer', function(){
   it('should be post data for GA.', function(done){
+    var sepia = require('sepia');
+    sepia.fixtureDir(path.join(process.cwd(), 'test', 'sepia-fixtures'));
+
     var args = {
       id: "UA-37457105-4",
       reportName: "TEST",
